21bdaa1d33dd1ccf51e2ea793a34ce85bbb05ccf
Ian Millard <steepleian@gmail.com>
2021-01-28 16:36:35 +0000

    Fixed PHP8.x.x compatibility issue

--- www/common.php
+++ www/common.php
@@ -4,7 +4,7 @@ date_default_timezone_set($TZ);
 //translations for HOMEWEATHERSTATION TEMPLATE UPDATED 2nd November added set locale
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
-mb_http_input('UTF-8');
+mb_http_input('G');
 mb_language('uni');
 mb_regex_encoding('UTF-8');
 ob_start('mb_output_handler');
