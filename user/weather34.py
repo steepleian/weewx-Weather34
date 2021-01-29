d31fc94790fb543c5dd43f512b70846dedd552fd
balloonhotair <51388216+balloonhotair@users.noreply.github.com>
2021-01-29 07:45:21 -0700

    New earthquake service

--- user/weather34.py
+++ user/weather34.py
@@ -34,8 +34,11 @@ import math
 import os
 import re
 import sys
+import json
 import time
 import weeutil.rsyncupload
+try: import xmltodict
+except: pass
 from distutils.version import StrictVersion
 try:
     import urllib2 as urllib
@@ -458,6 +461,8 @@ class ForecastData():
             while services:
                 service = services.pop(0)
                 url = self.settings_dict.get(service + "_url")
+                try: service_xml = self.settings_dict.get(service + "_xml_data", False)
+                except: service_xml = False
                 header = self.settings_dict.get(service + "_header", "User-Agent:Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.3 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/534.3").split(":")
                 header = {header[0]:":".join(header[1:])}
                 try:
@@ -472,10 +477,16 @@ class ForecastData():
                         os.mkdir(os.path.dirname(lfilename), 0o777)
                     loginf("Web Service: %s is running" % (service,))
                     try:
-                        response = urllib.urlopen(urllib.Request(url, None, header), timeout = 15)
+                        response = urllib.urlopen(urllib.Request(url, None, header), timeout = 15).read().decode('utf-8')
+                        if service_xml:
+                            try:
+                                response = json.dumps(xmltodict.parse(response))
+                            except Exception as err:
+                                response = ""
+                                logerr("Web service " + service + " Failed XML2Json parsing with error: " + err)
                         try:
                             with open(lfilename, 'w+') as file_handle:
-                                file_handle.write(str(response.read().decode('utf-8')))
+                                file_handle.write(str(response))
                         except Exception as err:
                             logerr("Error writing web service file: %s, Error: %s" % (lfilename, err))
                             continue
@@ -528,9 +539,9 @@ class CloudCover():
             logdbg("CloudCover File 1 " + file1)
             logdbg("CloudCover File 2 " + file2)
             while True:
-                logdbg("CloudCover url1 exit code " + str(os.system("wget -r -O " + "'" + file1 + "'" + " '" + url1 + "'")))
+                #logdbg("CloudCover url1 exit code " + str(os.system("wget -r -O " + "'" + file1 + "'" + " '" + url1 + "'")))
                 os.chmod(file1, 0o666)
-                logdbg("CloudCover url2 exit code " + str(os.system("wget -r -O " + "'" + file2 + "'" + " '" + url2 + "'")))
+                #logdbg("CloudCover url2 exit code " + str(os.system("wget -r -O " + "'" + file2 + "'" + " '" + url2 + "'")))
                 os.chmod(file2, 0o666)
                 if os.stat(file1).st_size > 5000 and os.stat(file2).st_size > 5000:
                     alt = weewx.almanac.Almanac(time.time(), float(lat), float(lon)).sun.alt
@@ -565,6 +576,8 @@ class CloudCover():
                         self.cloud_cover_percent = 1
                     if self.cloud_cover_percent > 99:
                         self.cloud_cover_percent = 99
+                    im = None
+                    pixarray = None
                 time.sleep(time_interval)
         except Exception as e:
             logdbg("CloudCover:calculate_cloud_cover " + str(e))
