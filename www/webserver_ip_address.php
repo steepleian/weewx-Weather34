a9bb6a857dc151395cd8adce7e5d31b79d59909b
balloonhotair <51388216+balloonhotair@users.noreply.github.com>
2021-02-28 09:03:21 -0700

    Added timeout to socket connect

--- www/webserver_ip_address.php
+++ www/webserver_ip_address.php
@@ -8,6 +8,7 @@
       $weewxserver_port = trim($data[1]);
       try {
           $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+          socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array('sec' => 2, 'usec' => 0));
           if (socket_connect($socket, $weewxserver_address, $weewxserver_port)){
             sleep(0.1);
             @socket_close($socket);
