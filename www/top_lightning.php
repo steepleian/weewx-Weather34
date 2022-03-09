7288fbff9d8e2692b2b635b93ad3778c311c9517
Rayvenhaus <steven.sheeley@gmail.com>
2022-03-09 20:23:13 +1100

    Update top_lightning.php
    Corrected comma error on line 12

--- www/top_lightning.php
+++ www/top_lightning.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 include('serverdata/lightningdata.php');
 include('settings.php');
 include('shared.php');
@@ -7,13 +7,9 @@ header('Content-type: text/html; charset=utf-8');
 error_reporting(0);
 ?>
 <body>
-
-
-
-
 <div class="wfstrike">
 <?php //weather34  lightning
- echo "<wfstriketoday>",$lightning3hr  ;
+ echo "<wfstriketoday>".$lightning3hr;
  ?></wfstriketoday></div>
 <div class="minwordl">Strikes</div></div>
 <div class="mintimedatex"><value>&nbsp;Last 3 Hrs<value></div>
@@ -22,17 +18,22 @@ error_reporting(0);
 //weather34 weather34 last detect
 if (isset($lastlightningtime)) {
 echo "<spanfeelstitle>Last Strike: <orange> ".date("j M Y", $lastlightningtime)." </orange> ";}?><br>
-<?php 
-if ($windunit == 'mph'){echo "<spanfeelstitle>Last Distance At:<orange> " .number_format($lightningdistance*0.621371,1). "  </orange>miles";}
-else  echo "<spanfeelstitle>Last Distance At:<orange> " .$lightningdistance. "  </orange>km";
-?><br>
+<?php
+if ($windunit == 'mph'){
+  echo "<spanfeelstitle>Last Distance At:<orange> " .number_format($lightningdistance*0.621371,1). "  </orange>miles";
+}else{
+  echo "<spanfeelstitle>Last Distance At:<orange> " .$lightningdistance. "  </orange>km";
+}
+?><br />
 
 <?php
 //weather34 weather34 last detect
-echo "<spanfeelstitle>Alltime Strike Total: <orange> ".$lightning." </orange> ";?><br>  
+echo "<spanfeelstitle>All-time Strike Total: <orange> ".$lightning." </orange> ";?><br>
   </div>
 <div class="lightningicon">
 <?php
  // display an icon when strike(s) are detected
-if ($weatherflow['lightning3hr'] > 0) echo '<img src="img/lightningalert.svg" width="20" height="20" align="right"/>';?>
+if ($weatherflow['lightning3hr'] > 0){
+  echo '<img src="img/lightningalert.svg" width="20" height="20" align="right"/>';
+}?>
 </div>
