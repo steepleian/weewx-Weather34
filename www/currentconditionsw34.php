2cbdf30ccd90cdfafac2052c3966ad1c2fa501fb
Ian Millard <18438654+steepleian@users.noreply.github.com>
2022-03-17 10:24:39 +0000

    updated cloud icons

--- www/currentconditionsw34.php
+++ www/currentconditionsw34.php
@@ -57,9 +57,9 @@ else if($weather["rain_rate"]>0){echo "<img rel='prefetch' src='css/svg/09.svg' 
 //else if($weather["rain_rate2"]>10){echo "<img rel='prefetch' src='css/svg/10.svg' width='70px' height='60px' alt='weather34 heavy rain icon'>";}
 //else if($weather["rain_rate2"]>0){echo "<img rel='prefetch' src='css/svg/09.svg' width='70px' height='60px' alt='weather34 rain icon'>";}
 //fog-weather34
-else if($weather["temp"] -$weather["dewpoint"] <0.8  && $now >$suns2 && $weather["temp"]>5){echo "<img rel='prefetch' src='css/svg/15.svg' width='70px' height='60px' alt='weather34 fog icon'>";}
-else if($weather["temp"] -$weather["dewpoint"] <0.8  && $now <$sunr2 && $weather["temp"]>5){echo "<img rel='prefetch' src='css/svg/15.svg' width='70px' height='60px' alt='weather34 fog icon'>";}
-else if($weather["temp"] -$weather["dewpoint"] <0.8  && $weather["temp"]>5){echo "<img rel='prefetch' src='css/svg/15.svg' width='70px' height='60px' alt='weather34 fog'>";}
+else if($weather["temp"] -$weather["dewpoint"] <0.8  && $now >$suns2 && $weather["temp"]>5){echo "<img rel='prefetch' src='css/svg/fog.night.svg' width='70px' height='60px' alt='weather34 fog icon'>";}
+else if($weather["temp"] -$weather["dewpoint"] <0.8  && $now <$sunr2 && $weather["temp"]>5){echo "<img rel='prefetch' src='css/svg/fog.day.svg' width='70px' height='60px' alt='weather34 fog icon'>";}
+else if($weather["temp"] -$weather["dewpoint"] <0.8  && $weather["temp"]>5){echo "<img rel='prefetch' src='css/svg/fog.svg' width='70px' height='60px' alt='weather34 fog'>";}
 //windy moderate-weather34
 else if($weather["wind_speed_avg"]<15 && $now <$sunr2 && $weather["cloud_cover"]<20){echo "<img rel='prefetch' src='css/svg/01n.svg' width='70px' height='60px' alt='weather34 windy icon'>";}
 else if($weather["wind_speed_avg"]<15 && $weather["cloud_cover"]<20){echo "<img rel='prefetch' src='css/svg/01d.svg' width='70px' height='60px' alt='weather34 windy icon'>";}
@@ -87,14 +87,14 @@ else if ($now <$sunrs2 ){echo "<img rel='prefetch' src='css/svg/02n.svg' width='
 else echo "<img rel='prefetch' src='css/svg/02d.svg' width='70px' height='60px' alt='weather34 windy icon'>"; 
 }
 else if ($weather["cloud_cover"]<60) {
-if ($now >$suns2 ){echo "<img rel='prefetch' src='css/svg/03n.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
-else if ($now <$sunrs2 ){echo "<img rel='prefetch' src='css/svg/03n.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
-else echo "<img rel='prefetch' src='css/svg/03d.svg' width='70px' height='60px' alt='weather34 windy icon'>"; 
+if ($now >$suns2 ){echo "<img rel='prefetch' src='css/svg/03n-part.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
+else if ($now <$sunrs2 ){echo "<img rel='prefetch' src='css/svg/03n-part.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
+else echo "<img rel='prefetch' src='css/svg/03d-part.svg' width='70px' height='60px' alt='weather34 windy icon'>"; 
 }
 else if ($weather["cloud_cover"]<80) {
-if ($now >$suns2 ){echo "<img rel='prefetch' src='css/svg/03n.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
-else if ($now <$sunrs2 ){echo "<img rel='prefetch' src='css/svg/03n.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
-else echo "<img rel='prefetch' src='css/svg/03d.svg' width='70px' height='60px' alt='weather34 windy icon'>"; 
+if ($now >$suns2 ){echo "<img rel='prefetch' src='css/svg/03n-most.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
+else if ($now <$sunrs2 ){echo "<img rel='prefetch' src='css/svg/03n-most.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
+else echo "<img rel='prefetch' src='css/svg/03d-most.svg' width='70px' height='60px' alt='weather34 windy icon'>"; 
 }
 else if($weather["cloud_cover"]>=80) {
 if ($now >$suns2 ){echo "<img rel='prefetch' src='css/svg/04.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
