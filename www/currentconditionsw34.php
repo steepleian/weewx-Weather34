0b9a9725999e8ae7fb84743b2caf26b335d55bcc
Ian Millard <18438654+steepleian@users.noreply.github.com>
2022-03-18 17:42:39 +0000

    Align current cloud conditions to AerisWeather cloud definitions

--- www/currentconditionsw34.php
+++ www/currentconditionsw34.php
@@ -12,6 +12,8 @@ error_reporting(0);
 $json_string             = file_get_contents("jsondata/me.txt");
 $parsed_json             = json_decode($json_string, true);
 $sky_code                = $parsed_json['data'][0]['clouds'][0]['code'];
+if ($windunit =='mph' ||  $windunit =='kts'){$weather["cloudbase3"]   = $parsed_json['data'][0]['clouds'][0]['feet'];}
+else if ($windunit =='km/h' ||  $windunit =='m/s'){$weather["cloudbase3"]   = $parsed_json['data'][0]['clouds'][0]['meters'];} 
 
 $result = date_sun_info(time(), $lat, $lon);$sunr=date_sunrise(time(), SUNFUNCS_RET_STRING, $lat, $lon, $rise_zenith, $UTC_offset);$suns=date_sunset(time(), SUNFUNCS_RET_STRING, $lat, $lon, $set_zenith, $UTC_offset);
 $sunr1=date_sunrise(strtotime('+1 day', time()), SUNFUNCS_RET_STRING, $lat, $lon, $rise_zenith, $UTC_offset);$suns1=date_sunset(strtotime('+1 day', time()), SUNFUNCS_RET_STRING, $lat, $lon, $set_zenith, $UTC_offset);
@@ -31,7 +33,7 @@ $visibility              = round($parsed_visibility['response'][0]['periods'][0]
 if ($cloud_region[0] !== "Europe"){$weather["cloud_cover"]  = $parsed_visibility['response'][0]['periods'][0]['sky'];}
 ?>
 <div class="updatedtimecurrent">
-<?php $forecastime=filemtime('jsondata/me.txt');$weather34wuurl = file_get_contents("jsondata/me.txt");if(filesize('jsondata/me.txt')<10){echo  $online;}
+<?php $forecastime=filemtime('jsondata/awc.txt');$weather34wuurl = file_get_contents("jsondata/awc.txt");if(filesize('jsondata/awc.txt')<10){echo  $online;}
 else echo $online,"";echo " ",	date($timeFormat,$forecastime);	?></div>
 <div class="cloudconverter">
 <?php //cloudbase-weather34
@@ -61,8 +63,7 @@ else if($weather["temp"] -$weather["dewpoint"] <0.8  && $now >$suns2 && $weather
 else if($weather["temp"] -$weather["dewpoint"] <0.8  && $now <$sunr2 && $weather["temp"]>5){echo "<img rel='prefetch' src='css/svg/fog.day.svg' width='70px' height='60px' alt='weather34 fog icon'>";}
 else if($weather["temp"] -$weather["dewpoint"] <0.8  && $weather["temp"]>5){echo "<img rel='prefetch' src='css/svg/fog.svg' width='70px' height='60px' alt='weather34 fog'>";}
 //windy moderate-weather34
-else if($weather["wind_speed_avg"]<15 && $now <$sunr2 && $weather["cloud_cover"]<20){echo "<img rel='prefetch' src='css/svg/01n.svg' width='70px' height='60px' alt='weather34 windy icon'>";}
-else if($weather["wind_speed_avg"]<15 && $weather["cloud_cover"]<20){echo "<img rel='prefetch' src='css/svg/01d.svg' width='70px' height='60px' alt='weather34 windy icon'>";}
+else if($weather["wind_speed_avg"]>=15 && $now >$suns2 && $weather["cloud_cover"]<20){echo "<img rel='prefetch' src='css/svg/01nw.svg' width='70px' height='60px' alt='weather34 windy icon'>";}
 else if($weather["wind_speed_avg"]>=15 && $now <$sunr2 && $weather["cloud_cover"]<20){echo "<img rel='prefetch' src='css/svg/01nw.svg' width='70px' height='60px' alt='weather34 windy icon'>";}
 else if($weather["wind_speed_avg"]>=15 && $weather["cloud_cover"]<20){echo "<img rel='prefetch' src='css/svg/01dw.svg' width='70px' height='60px' alt='weather34 windy icon'>";}
 //windy moderate-weather34
@@ -71,32 +72,27 @@ else if($weather["wind_speed_avg"]>=15 && $now <$sunr2){echo "<img rel='prefetch
 else if($weather["wind_speed_avg"]>=15){echo "<img rel='prefetch' src='css/svg/wind.svg' width='70px' height='60px' alt='weather34 windy icon'>";}
 
 //cloud-icon
-else if ($weather["cloud_cover"]<5 and $weather["cloud_cover"]>0) {
+else if ($weather["cloud_cover"]<7 and $weather["cloud_cover"]>0) {
 if ($now >$suns2 ){echo "<img rel='prefetch' src='css/svg/01n.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
 else if ($now <$sunrs2 ){echo "<img rel='prefetch' src='css/svg/01n.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
 else echo "<img rel='prefetch' src='css/svg/01d.svg' width='70px' height='60px' alt='weather34 windy icon'>"; 
 } 
-else if ($weather["cloud_cover"]<20) {
+else if ($weather["cloud_cover"]<32) {
 if ($now >$suns2 ){echo "<img rel='prefetch' src='css/svg/02n.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
 else if ($now <$sunrs2 ){echo "<img rel='prefetch' src='css/svg/02n.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
 else echo "<img rel='prefetch' src='css/svg/02d.svg' width='70px' height='60px' alt='weather34 windy icon'>"; 
 }
-else if ($weather["cloud_cover"]<40) {
-if ($now >$suns2 ){echo "<img rel='prefetch' src='css/svg/02n.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
-else if ($now <$sunrs2 ){echo "<img rel='prefetch' src='css/svg/02n.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
-else echo "<img rel='prefetch' src='css/svg/02d.svg' width='70px' height='60px' alt='weather34 windy icon'>"; 
-}
-else if ($weather["cloud_cover"]<60) {
+else if ($weather["cloud_cover"]<70) {
 if ($now >$suns2 ){echo "<img rel='prefetch' src='css/svg/03n-part.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
 else if ($now <$sunrs2 ){echo "<img rel='prefetch' src='css/svg/03n-part.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
 else echo "<img rel='prefetch' src='css/svg/03d-part.svg' width='70px' height='60px' alt='weather34 windy icon'>"; 
 }
-else if ($weather["cloud_cover"]<80) {
+else if ($weather["cloud_cover"]<95) {
 if ($now >$suns2 ){echo "<img rel='prefetch' src='css/svg/03n-most.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
 else if ($now <$sunrs2 ){echo "<img rel='prefetch' src='css/svg/03n-most.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
 else echo "<img rel='prefetch' src='css/svg/03d-most.svg' width='70px' height='60px' alt='weather34 windy icon'>"; 
 }
-else if($weather["cloud_cover"]>=80) {
+else if($weather["cloud_cover"]>=95) {
 if ($now >$suns2 ){echo "<img rel='prefetch' src='css/svg/04.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
 else if ($now <$sunrs2 ){echo "<img rel='prefetch' src='css/svg/04.svg' width='70px' height='60px' alt='weather34 windy icon'>";} 
 else echo "<img rel='prefetch' src='css/svg/04.svg' width='70px' height='60px' alt='weather34 windy icon'>"; 
@@ -131,12 +127,15 @@ else if($weather["wind_speed_avg"]>=30){echo "Very Windy ".$alert."<br>Condition
 else if($weather["wind_speed_avg"]>=22){echo "Moderate Wind <br>Conditions";}
 else if($weather["wind_speed_avg"]>=15){echo "Breezy <br>Conditions";}
 //cloud-description
-else if($weather["cloud_cover"]<5 and $weather["cloud_cover"]>0) {echo "Clear <br>Conditions";}
-else if($weather["cloud_cover"]<20) {echo "Mostly Clear <br>Conditions";}
-else if($weather["cloud_cover"]<40) {echo "Scattered <br>Clouds";}
-else if($weather["cloud_cover"]<60) {echo "Partly Cloudy <br>Conditions";}
-else if($weather["cloud_cover"]<80) {echo "Mostly Cloudy <br>Conditions";}
-else if($weather["cloud_cover"]>=80) {echo "Overcast <br>Conditions";}
+else if($weather["cloud_cover"]<7 and $weather["cloud_cover"]>0) {echo "Clear <br>Conditions";}
+else if ($weather["cloud_cover"]<32) {
+if ($now >$suns2 ){echo "Mostly Clear <br>Conditions";} 
+else if ($now <$sunrs2 ){echo "Mostly Clear <br>Conditions";} 
+else echo "Mostly Sunny <br>Conditions"; 
+}
+else if($weather["cloud_cover"]<70) {echo "Partly Cloudy <br>Conditions";}
+else if($weather["cloud_cover"]<95) {echo "Mostly Cloudy <br>Conditions";}
+else if($weather["cloud_cover"]>=95) {echo "Overcast <br>Conditions";}
 else if(filesize('jsondata/me.txt')<160){echo "<uppercase>Conditions<br>Not Available</uppercase>";}
 //oktas
 if($weather["cloud_cover"]<5 and $weather["cloud_cover"]>0) {$weather["cloud_oktas"]="0 oktas";}
@@ -155,21 +154,22 @@ else if($weather["cloud_cover"]<=100) {$weather["cloud_oktas"]="8 oktas";}
  <!-- weather34 generated Data--> 
 <div class="darkskynexthours" style="margin: 60px auto auto">
 <?php //weather34 average station data
-//echo "Average <oblue>Cloud Cover</oblue> last 5 min <ogreen>" .$weather["cloud_cover"]."</ogreen><valuetext>".$cloudcoverunit. "(".$weather["cloud_oktas"].")";
-echo "<oorange>Visibility </oorange>".$visibility."</br>";
+//echo "Average <oblue>Cloud Cover</oblue> last 5 minutes <ogreen>" .$weather["cloud_cover"]."</ogreen><valuetext>".$cloudcoverunit. "(".$weather["cloud_oktas"].")";
+echo "Visibility ".$visibility."</br>";
 
 if (strpos($weather["cloud_cover"],"N/A") == false){
 echo "<oblue>Cloud Cover</oblue><ogreen> " .$weather["cloud_cover"]."</ogreen><valuetext>".$cloudcoverunit. " (".$weather["cloud_oktas"].")";
 }
-
-echo "<br>Average <oorange>Temperature</oorange> last 60 min ";if($weather["temp_avg"]>=20){echo "<oorange>" .$weather["temp_avg"]."</oorange>°<valuetext>".$tempunit;} else if($weather["temp_avg"]<=10){echo "<oblue>" .$weather["temp_avg"]."</oblue>°<valuetext>".$tempunit;}else if($weather["temp_avg"]<20){echo "<ogreen>" .$weather["temp_avg"]."</ogreen>°<valuetext>".$tempunit;}echo "</valuetext><br>";
+echo "<br>Average <oorange>Temperature</oorange> last 60 minutes ";if($weather["temp_avg"]>=20){echo "<oorange>" .$weather["temp_avg"]."</oorange>°<valuetext>".$tempunit;} else if($weather["temp_avg"]<=10){echo "<oblue>" .$weather["temp_avg"]."</oblue>°<valuetext>".$tempunit;}else if($weather["temp_avg"]<20){echo "<ogreen>" .$weather["temp_avg"]."</ogreen>°<valuetext>".$tempunit;}echo "</valuetext><br>";
 echo  "Max <oblue>Wind Gust</oblue> ";
 if ($windunit=='kts'){$windunit="kn";}
-
+//if ($windunit=='kn'){$weather["wind_gust_60min"] = number_format(1.94384*$weather["wind_gust_60min"], 1);}
+//else if ($windunit=='km/h'){$weather["wind_gust_60min"] = number_format(3.6*$weather["wind_gust_60min"], 1);}
+//else if ($windunit=='mph'){$weather["wind_gust_60min"] = number_format(2.236936*$weather["wind_gust_60min"], 1);}
 if($weather["wind_gust_60min"]>=50){echo "<ored>" ,number_format(round($weather["wind_gust_60min"],1))."</ored> ".$windunit;}
 else if($weather["wind_gust_60min"]>=30){echo "<oorange>" ,number_format(round($weather["wind_gust_60min"],1))."</oorange><valuetext> ".$windunit;}
 else if($weather["wind_gust_60min"]>=0){echo "<ogreen>" ,number_format(round($weather["wind_gust_60min"],1))."</ogreen><valuetext> ".$windunit;}echo " </valuetext>last 60 minutes ";
-echo  " <br>Average <oblue>Wind Speed</oblue> last 10 min ";if($weather["wind_speed_avg"]>=30){echo "<ored>" ,number_format(round($weather["wind_speed_avg"]))."</ored> ".$windunit;}else if($weather["wind_speed_avg"]>=20){echo "<oorange>" .$weather["wind_speed_avg"]."</oorange><valuetext> ".$windunit;}
+echo  " <br>Average <oblue>Wind Speed</oblue> last 10 minutes ";if($weather["wind_speed_avg"]>=30){echo "<ored>" ,number_format(round($weather["wind_speed_avg"]))."</ored> ".$windunit;}else if($weather["wind_speed_avg"]>=20){echo "<oorange>" .$weather["wind_speed_avg"]."</oorange><valuetext> ".$windunit;}
 else if($weather["wind_speed_avg"]>=0){echo "<ogreen>" ,number_format(round($weather["wind_speed_avg"]))."</ogreen><valuetext> ".$windunit;}
 echo "</valuetext>";
 if($weather["wind_direction_avg"]>0){echo "<br>Average Direction <oorange>"; if($weather["wind_direction_avg"]<=11.25){echo "North";}else if($weather["wind_direction_avg"]<=33.75){echo "NNE";}else if($weather["wind_direction_avg"]<=56.25){echo "NE";}else if($weather["wind_direction_avg"]<=78.75){echo "ENE";}else if($weather["wind_direction_avg"]<=101.25){echo "East";}else if($weather["wind_direction_avg"]<=123.75){echo "ESE";}else if($weather["wind_direction_avg"]<=146.25){echo "SE";}
